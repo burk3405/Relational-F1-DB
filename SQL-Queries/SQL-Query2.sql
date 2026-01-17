3c34557369c759ecb785973230c8c9a3f9c34001
burk3405 <acburkett04@gmail.com>
2026-01-16 19:13:57 -0500

    Create SQL-Query2.sql

---
+++ SQL-Queries/SQL-Query2.sql
@@ -0,0 +1,16 @@
+-- Project- Formula One Driver Trivia Database
+-- Aaron Burkett
+-- 12/21/2025
+
+-- Database Population Queries
+
+create database Formula1;
+
+use Formula1;
+
+insert into Drivers (DriverID, FirstName, MiddleName, LastName, CurrentlyRacing, BirthCountry, DateOfBirth, Nationality, Team2025, Team2026, CarNumber, TotalEntries, TotalChampionships, TotalWins, TotalPodiums, CareerPoints, PolePositions, FastestLaps, FirstEntry, FirstWin, LastWin, LastEntry, WDC2025Position, WDC2025Points)
+values
+	('F03923405', 'Max', 'Emilian', 'Verstappen', 1, 'Belgium', 1997-09-30, 'Dutch', 'Red Bull Racing-Honda RBPT', 'Red Bull Racing-Red Bull Ford', 3, 233, 4, 71, 127, 3444.5, 48, 36, '2015 Australian Grand Prix', '2016 Spanish Grand Prix', '2025 Abu Dhabi Grand Prix', '2025 Abu Dhabi Grand Prix', 2, 421),
+
+
+
